6601fc88d607a0bba221e48ec1b1e0be6ec62221
jjuyoung00 <55523928+jjuyoung00@users.noreply.github.com>
2020-03-21 09:50:16 +0900

    Add files via upload

---
+++ 1번 네모.cpp
@@ -0,0 +1,14 @@
+#include<stdio.h>
+int main(void)
+{
+	printf("╡╡╟№бс");
+	printf("бс бс бс бс бс
+            бс бс
+            бс бс
+            бс бс бс бс бс") ;
+           
+           
+           
+           
+           return 0;
+}
---
+++ 2번 세모.cpp
@@ -0,0 +1,14 @@
+#include <stdio.h>
+ 
+int main( void )
+{
+       for (int i = 1 ; i < 6 ; i = i + 1)
+       {
+               for (int j = 0 ; j < i ; j = j+1)
+               {
+                     printf("¡â");
+               }
+               printf("\n");
+        }
+        return 0;
+}
