85823169ae6a35558e375862cbd139fdd03fa3d5
jjuyoung00 <55523928+jjuyoung00@users.noreply.github.com>
2020-06-08 15:42:14 +0900

    Update 2번 세모.cpp

--- 2번 세모.cpp
+++ 2번 세모.cpp
@@ -1,14 +1,24 @@
-#include <stdio.h>
- 
-int main( void )
+#include<stdio.h>  
+int main(void) 
 {
-       for (int i = 1 ; i < 6 ; i = i + 1)
-       {
-               for (int j = 0 ; j < i ; j = j+1)
-               {
-                     printf("¡â");
-               }
-               printf("\n");
-        }
-        return 0;
-}
+	unsigned char 
+	a1=0xa1;
+	
+	unsigned char 
+	a2=0xe2;
+	
+	int i, j;
+	
+	for(i=1;i<=5;i++)
+	{
+		
+	for(j=1;j<=i;j++)
+	
+	printf("%c%c",a1, a2);
+	
+	printf("\n");
+	
+	}
+	
+	return 0; 
+	}
